2a551e9e24481a19cd52917b3962b65078feab21
madisonhagio <hagiom@hawaii.edu>
2022-03-02 22:45:02 -1000

    Parts 1-7 complete

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+//Part 4: Created IBOutlets
+    @IBOutlet var topLabel: UILabel!
+    
+    @IBOutlet var bottomLabel: UILabel!
+    //Part 6
+    var currentIndex = 0
+    
+    var favoriteFoodsArray = ["Burgers", "Sushi", "Laulau", "Poke bowls", "Ice cream"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Part 5: Set topLabel's text to "My Favorite Foods"
+        topLabel.text = "My Favorite Foods"
+        
+        bottomLabel.text = favoriteFoodsArray[currentIndex]
     }
 
+//Part 8
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if self.currentIndex < favoriteFoodsArray[currentIndex]
+            buttonLabel.selfTitle("Next", for: UIControl.State.normal)
+            currentIndex += 1
+    }
+    else
 }
-
+print("button has been disabled")
+(buttonLabel!).isEnabled = false 
